83961350aad215d8918ee687b6510a98a0efa037
Jack Moxon <jsmoxon@gmail.com>
2012-05-10 20:44:53 -0700

    added url line to permit static files on heroku

--- blockrsite/urls.py
+++ blockrsite/urls.py
@@ -9,3 +9,7 @@ urlpatterns = patterns('',
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+urlpatterns += patterns('django.contrib.staticfiles.views',
+    url(r'^static/(?P<path>.*)$', 'serve', kwargs={"insecure": True}),
+)
