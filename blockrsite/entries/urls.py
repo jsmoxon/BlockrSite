be0a2c483e056ac5f6fdd819534d0eb367151bf4
Jack Moxon <jsmoxon@gmail.com>
2012-05-13 17:25:04 -0700

    added google site verification

--- blockrsite/entries/urls.py
+++ blockrsite/entries/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.views.generic.simple import direct_to_template
 
 urlpatterns = patterns('',
 #    url(r'^home/', 'entries.views.home'),
@@ -9,4 +10,5 @@ urlpatterns = patterns('',
     url(r'^entries/', 'entries.views.list'),                       
     url(r'^flag/', 'entries.views.flag'),
     url(r'^(?P<entry_id>\d+)/', 'entries.views.view'),                       
+    url(r'^google/', direct_to_template, {'template': 'google.html'}),                        
 )
