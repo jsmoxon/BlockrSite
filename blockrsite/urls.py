26157037e71e80e5b4cdbea44c4b0c97d3e8fa84
Jack Moxon <jsmoxon@gmail.com>
2012-05-18 16:48:00 -0700

    added login box to home page and improved logout redirect url so that it just goes to the home page

--- blockrsite/urls.py
+++ blockrsite/urls.py
@@ -9,7 +9,7 @@ urlpatterns = patterns('',
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),
-    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'template_name': 'home.html'}),
+    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
 )
 
 urlpatterns += patterns('django.contrib.staticfiles.views',
