c72c3ee34b53b1bae2dc3fe7e36bb9d151c578ae
Jack Moxon <jsmoxon@gmail.com>
2012-07-06 00:06:34 -0700

    added pw reset

--- blockrsite/settings.py
+++ blockrsite/settings.py
@@ -1,4 +1,4 @@
-# Django settings for blockrsite project.
+from constants import *
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
--- blockrsite/urls.py
+++ blockrsite/urls.py
@@ -12,6 +12,10 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),
     url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
+    url(r'^accounts/', include('django.contrib.auth.urls')),
+#    url(r'^accounts/password_reset/', 'django.contrib.auth.views.password_reset', {'template_name': 'registration/pw_reset_form.html'}),   
+#    url(r'^accounts/reset/done/', 'django.contrib.auth.views.password_reset_done', {'template_name': 'home2.html'}),
+#    url(r'^accounts/
 )
 
 
