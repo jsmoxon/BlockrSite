5dd0f50b2725974611aa0af58689f64cbafa370b
Jack Moxon <jsmoxon@gmail.com>
2012-07-18 17:17:48 -0700

    rm'd

--- blockrsite/constants.py
+++
@@ -1,9 +0,0 @@
-PARENT_HOST = 'www.blockr.me'
-SITE_ID = "www.blockr.me"
-DOMAIN = 'www.blockr.me'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'blockremail@gmail.com'
-EMAIL_HOST_PASSWORD = 'blockremail'
-EMAIL_USE_TLS = True
-SECRET_KEY = ''
